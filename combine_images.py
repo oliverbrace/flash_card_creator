bd36f79df32dbbd9b9f7fa10f8d5e7e268b52b69
oliverbrace <42202976+oliverbrace@users.noreply.github.com>
2024-02-14 20:32:21 +0000

    Better combine

--- combine_images.py
+++ combine_images.py
@@ -6,7 +6,10 @@ from utils.serialize_image import ImageSerialize
 def run():
     images = get_all_images()
     a4_images = combine_images_into_grid(images)
-    ImageSerialize().output_image(a4_images[0], "test.png", "images/flash_cards_a4")
+    for i, a4_image in enumerate(a4_images):
+        ImageSerialize().output_image(
+            a4_image, f"a4_image_{i}.png", "images/flash_cards_a4"
+        )
 
 
 if __name__ == "__main__":
